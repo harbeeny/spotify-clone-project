63d548afd62c75dc1425bb16e85831cce52296bb
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-01-22 13:09:18 -0500

    Configure Sidebar

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="text-green-500">
-      Hello Spotifyy
+      Main Content
     </div>
   )
 }
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SidebarItem() {
+  return (
+    <div>Sidebar Item</div>
+  )
+}
+
+export default SidebarItem
