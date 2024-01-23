7ffd68bb6612ce84c2af52f054bb0db5b8961dbd
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-01-23 18:08:05 -0500

    Implemented Modal functionality

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 import Modal from "./Modal";
 
-
 function AuthModal() {
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
+    const { session } = useSessionContext();
+
   return (
     <Modal
         title="Welcome back"
@@ -11,7 +19,23 @@ function AuthModal() {
         isOpen
         onChange={() => {}}
     >
-        AuthModal children!
+        <Auth 
+            theme= "dark"
+            magicLink
+            providers={["github", "google", "spotify"]}
+            supabaseClient={supabaseClient}
+            appearance= {{
+                theme: ThemeSupa,
+                variables: {
+                    default: {
+                        colors: {
+                            brand: '#404040',
+                            brandAccent: '#22c55e'
+                        }
+                    }
+                }
+            }}
+        />
     </Modal>
   )
 }
