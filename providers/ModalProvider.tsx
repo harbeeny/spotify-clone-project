a87d14aad47705bdbb1b11ac28c3c40d5118be0a
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-01-23 17:46:07 -0500

    Built Modal component

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useEffect, useState } from 'react'
+
+import Modal from '@/components/Modal';
+
+const ModalProvider= () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+        <Modal
+            title="Test Modal"
+            description="test Description"
+            isOpen
+            onChange={() => {}}
+        >
+            Test Children
+        </Modal>
+
+    </>    
+  )
+}
+
+export default ModalProvider;
