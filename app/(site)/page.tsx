3ca2648c065aa4483c2f64ed41ff79f3a07c65cd
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-01-22 21:37:19 -0500

    Layout setup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,10 +11,24 @@ export default function Home() {
             Welcome back
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-              <ListItem />
+              <ListItem
+                image="/images/liked.png"
+                name="Liked Song"
+                href="liked"
+              />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
     </div>
   )
 }
