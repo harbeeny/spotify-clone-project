1e935f9031fad850a2c7e82e4e1181f1ef058fd5
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-01-27 18:44:33 -0500

    Creating volume slider

---
+++ components/Slider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import * as RadixSlider from "@radix-ui/react-slider";
+
+interface SlideProps {
+
+}
+
+const Slider = () => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default Slider;
