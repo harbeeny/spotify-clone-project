db24b6c0ccd19bead013014b8581654686dd7e69
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-01-27 19:12:31 -0500

    Added functionality and logic to onclick for next and previous song

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -3,11 +3,13 @@
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import { useState } from "react";
 
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
 
 interface PlayerContentProps {
     song: Song;
@@ -18,8 +20,35 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     song,
     songUrl,
 }) => {
-    const Icon = true ? BsPauseFill : BsPlayFill;
-    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+    const player = usePlayer();
+    const [volume, setVolume] = useState(1);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = volume ? HiSpeakerXMark : HiSpeakerWave;
+
+    const onPlayNext = () => {
+        if(player.ids.length === 0) return;
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const nextSong = player.ids[currentIndex + 1];
+
+        if(!nextSong) return player.setId(player.ids[0]);
+
+        player.setId(nextSong); 
+    }
+
+    const onPlayPrevious = () => {
+        if(player.ids.length === 0) return;
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const previousSong = player.ids[currentIndex - 1];
+
+        if(!previousSong) return player.setId(player.ids[player.ids.length - 1]);
+
+        player.setId(previousSong);
+    }
+
     return ( 
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
             <div className="flex w-full justify-start">
@@ -38,7 +67,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 
             <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
                 <AiFillStepBackward  
-                    onClick={() => {}}
+                    onClick={onPlayPrevious}
                     size={30} 
                     className="text-neutral-400 cursor-pointer hover:text-white transition"
                     /> 
@@ -49,7 +78,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                     <Icon size={30} className="text-black" />
                 </div>
                 <AiFillStepForward 
-                    onClick={() => {}}
+                    onClick={onPlayNext}
                     size={30} 
                     className="text-neutral-400 cursor-pointer hover:text-white transition"
                 />
@@ -65,7 +94,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 </div>
             </div>
         </div>
-     );
+     );  
 }
- 
+
 export default PlayerContent;
