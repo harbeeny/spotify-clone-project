39e4d1f7d2e22663e0759e034422fe710e9b0d34
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-02-01 21:32:25 -0500

    Update AccountContent.tsx

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -50,7 +50,9 @@ const AccountContent = () => {
             {subscription && (
                <div className="flex flex-col gap-y-4">
                 <p>
-                    You are currently on the <b>{subscription?.prices?.products?.name}</b> plan.
+                    You are currently on the 
+                    <b>{subscription?.prices?.products?.name}</b> 
+                    plan.
                 </p>
                 <Button 
                     className="w-[300px]"
